4cc2bd328c7171d9f32f505ade85802132f15187
Nashiria <35883865+Nashiria@users.noreply.github.com>
2021-04-06 14:23:58 +0300

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -2,7 +2,7 @@ def pyramid(ls):
     floorCount = 0
     numcount = 0
     order=0
-    while numcount != len(ls):
+    while numcount < len(ls):
         numcount += (floorCount) + 1
         floorCount += 1
     spaceCount = floorCount - 1
@@ -11,8 +11,14 @@ def pyramid(ls):
             print(end=" ")
         spaceCount = spaceCount - 1
         for j in range(0, i + 1):
-            print(str(ls[order])+" ", end="")
-            order+=1
+            try:
+                print(str(ls[order])+" ", end="")
+                order+=1
+            except:
+                print("*" + " ", end="")
         print("\r")
-ls=[6,6,9,2,1,3,7,4,4,5]
-pyramid(ls)
+strx=""
+with open("data.txt","r") as file:
+    for line in file:
+        strx+=line.replace("\n","")
+pyramid(strx.split(","))
